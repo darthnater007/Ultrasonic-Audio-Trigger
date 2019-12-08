22871c6093d1c31423044cbc13acd7d0a0fad2e2
darthnater007 <nsperk@gmail.com>
2019-12-08 12:15:50 -0500

    API call added for phrase in tts.py

--- tts.py
+++ tts.py
@@ -1,17 +1,19 @@
-#install google tts with `pip install gTTS`
-from gtts import gTTS
-
 from gtts import gTTS 
 
 import os
-    
+
+import requests
+
+
+def get_phrase():
+    response = requests.get("https://uat-server-jsu25stwna-uc.a.run.app/getphrase")
+    print(str(response.json()))
+    return str(response.json())
+
 def play_audio():
-    # TODO: have this call text from an API
-    my_text = "Reminder: get Nate a cup of coffee."
-    #get tts response
-    myobj = gTTS(text=my_text, lang='en', slow=False) 
-    print(myobj)
-    #save it
-    myobj.save("./audio/audio.mp3")
-    #play it
+    my_text = get_phrase()
+    my_object = gTTS(text=my_text, lang='en', slow=False) 
+    my_object.save("./audio/audio.mp3")
     os.system("mpg123 ./audio/audio.mp3")
+
+play_audio()
